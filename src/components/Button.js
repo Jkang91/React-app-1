861b1f7a87a0c28c9e663e7951b96e9068fd2b93
jkang91 <jkang34@gmail.com>
2021-10-18 22:40:09 -0400

    Herosection

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ const STYLES = ['btn--primary', 'btn--outline'];
 
 const SIZES = ['btn--medium', 'btn--large'];
 
-const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
+export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
@@ -22,4 +22,3 @@ const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
     )
 };
 
-export default Button;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Button from './Button';
+import { Button } from './Button';
 import './Nav.css';
 
 
@@ -24,12 +24,17 @@ function Nav() {
         }
     }
 
+    useEffect(() => {
+        showButton()
+    },[])
+
     window.addEventListener('resize', showButton)
+
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                         TRVL <i className="fab fa-typo3" />
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
