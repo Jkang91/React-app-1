e70168a851b2a89299a09f52fde551125a1f930d
jkang91 <jkang34@gmail.com>
2021-10-19 23:08:39 -0400

    cards almost done

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,14 @@
-import '../../App.css';
-import HeroSection from '../HeroSection';
+import "../../App.css";
+import HeroSection from "../HeroSection";
+import Cards from '../Cards';
 
-
-function Home () {
-    return (
-        <>
-            <HeroSection />
-        </>
-    )
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+    </>
+  );
 }
 
 export default Home;
